fd05bdb368817cd048ac8e71de6d1da431d8bf6f
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-11-23 14:45:02 +0200

    bonus add search filter and sort
    processData function have to be optimised if there is a lot of data.

--- 40minuteTasks/productsTable.js/script.js
+++ 40minuteTasks/productsTable.js/script.js
@@ -1,5 +1,58 @@
 let data = [];
+let filterBy = "All";
+let sortBy = "name";
+let searchQuery = "";
+
 const tBodyElement = document.querySelector("tbody");
+const sortSelectElement = document.getElementById("sortBy");
+const filterSelectElement = document.getElementById("filter");
+const searchInputElement = document.querySelector("input");
+
+const sortData = (sortBy, data) => {
+	let sortedData = [];
+	if (sortBy == "name") {
+		sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
+	} else {
+		sortedData = data.sort((a, b) => {
+			if (sortBy == "asc") {
+				return a.price - b.price;
+			} else {
+				return b.price - a.price;
+			}
+		});
+	}
+
+	return sortedData;
+};
+
+const filterData = () => {
+	filterBy = filterBy[0].toUpperCase() + filterBy.slice(1, filterBy.length);
+
+	if (filterBy == "All") {
+		return data;
+	}
+
+	const filteredData = data.filter((el) => el.category == filterBy);
+
+	return filteredData;
+};
+
+const processData = (data) => {
+	let result = data;
+	result = filterData();
+
+	if (searchQuery != "") {
+		console.log("asd");
+		result = result.filter((el) =>
+			el.name.toLowerCase().includes(searchQuery.toLowerCase())
+		);
+	}
+
+	result = sortData(sortBy, result);
+
+	return result;
+};
+
 const getData = async () => {
 	try {
 		const response = await fetch("./data.json");
@@ -25,7 +78,10 @@ const createProduct = (product) => {
 };
 
 const renderProducts = (data) => {
-	data.forEach((product) => {
+	const result = processData(data);
+	tBodyElement.innerHTML = "";
+
+	result.forEach((product) => {
 		const productElement = createProduct(product);
 		tBodyElement.appendChild(productElement);
 	});
@@ -37,5 +93,22 @@ const startApp = async (data) => {
 
 	renderProducts(data);
 };
+const sortSelectChangeHandler = (e) => {
+	sortBy = e.target.value;
+	renderProducts(data);
+};
 
+const filterSelectChangeHandler = (e) => {
+	filterBy = e.target.value;
+	renderProducts(data);
+};
+
+const searchInputKeyupHandler = (e) => {
+	searchQuery = e.target.value;
+	renderProducts(data);
+};
+
+sortSelectElement.addEventListener("change", sortSelectChangeHandler);
+filterSelectElement.addEventListener("change", filterSelectChangeHandler);
+searchInputElement.addEventListener("keyup", searchInputKeyupHandler);
 startApp(data);
