d02167dada787e0ace029214ab1311eb858de399
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-12-08 15:42:44 +0200

    is prime task

---
+++ codeWars/6kyuTasks/fundamentals/isNumberPrime.js
@@ -0,0 +1,17 @@
+const isPrime = (n) => {
+	if (n <= 1) {
+		return false;
+	}
+
+	for (let i = 2; i * i <= n; i++) {
+		if (n % i === 0) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(-1));
