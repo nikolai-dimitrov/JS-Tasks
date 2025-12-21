ba75c4fda5e823ea08f0e089017fdd5b8185f854
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-12-21 10:54:20 +0200

    find file in file system

--- codeWars/6kyuTasks/recursion/sumTheTree.js
+++ codeWars/6kyuTasks/recursion/sumTheTree.js
@@ -3,7 +3,7 @@ const sumTheTree = (root) => {
 		return 0;
 	}
 
-    let result = root.value;
+	let result = root.value;
 
 	result += sumTheTree(root.left);
 	result += sumTheTree(root.right);
@@ -11,18 +11,22 @@ const sumTheTree = (root) => {
 	return result;
 };
 
-console.log(sumTheTree({
-	value: 10,
-	left: { value: 1, left: null, right: null },
-	right: { value: 2, left: null, right: null },
-}));
+console.log(
+	sumTheTree({
+		value: 10,
+		left: { value: 1, left: null, right: null },
+		right: { value: 2, left: null, right: null },
+	})
+);
 
-console.log(sumTheTree({
-	value: 11,
-	left: { value: 0, left: null, right: null },
-	right: {
-		value: 0,
-		left: null,
-		right: { value: 1, left: null, right: null },
-	},
-}));
+console.log(
+	sumTheTree({
+		value: 11,
+		left: { value: 0, left: null, right: null },
+		right: {
+			value: 0,
+			left: null,
+			right: { value: 1, left: null, right: null },
+		},
+	})
+);
